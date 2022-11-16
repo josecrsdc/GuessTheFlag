a0c3959fac0e26393d8ab9613a9bd5da844bfef9
Jose carlos Rodriguez <josecrsdc@gmail.com>
2022-11-16 18:06:04 +0100

    reorder view

--- GuessTheFlag/UI/ContentView.swift
+++ GuessTheFlag/UI/ContentView.swift
@@ -12,47 +12,16 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-            ], center: .top, startRadius: 200, endRadius: 400)
-                .ignoresSafeArea()
+            backgroundRadialGradient
             VStack(spacing: 30) {
+                guessFlagTitle
                 VStack {
-                    Spacer()
-                    Text("Toca la bandera de")
-                        .font(.subheadline.weight(.heavy))
-                    Text(vm.countries[vm.correctAnswer])
-                        .font(.largeTitle.weight(.semibold))
-                }
-                .foregroundColor(.white)
-                
-                VStack {
-                    Text("Adivina la bandera")
-                            .font(.largeTitle.bold())
-                            .foregroundColor(.white)
-                            .padding(.bottom, 60)
-                    
-                    VStack(spacing: 15) {
-                        ForEach(0..<3) { number in
-                            Button {
-                                vm.flagTapped(number)
-                            } label: {
-                                Image(vm.countries[number])
-                                    .flagStyle()
-                            }
-                        }
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 20)
-                    .background(.regularMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    fixedTitle
+                    flagStack
                 }
                 Spacer()
                 Spacer()
-                Text("Puntuación: \(vm.score)")
-                    .foregroundColor(.white)
-                    .font(.title.bold())
+                scoreView
             }
             .padding()
         }
@@ -69,7 +38,58 @@ struct ContentView: View {
     }
 }
 
-struct GuessTheFlag_Previews: PreviewProvider {
+extension ContentView {
+    private var backgroundRadialGradient: some View {
+        RadialGradient(stops: [
+            .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+            .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+        ], center: .top, startRadius: 200, endRadius: 400)
+            .ignoresSafeArea()
+    }
+    
+    private var guessFlagTitle: some View {
+        VStack {
+            Spacer()
+            Text("Toca la bandera de")
+                .font(.subheadline.weight(.heavy))
+            Text(vm.countries[vm.correctAnswer])
+                .font(.largeTitle.weight(.semibold))
+        }
+        .foregroundColor(.white)
+    }
+    
+    private var fixedTitle: some View {
+        Text("Adivina la bandera")
+                .font(.largeTitle.bold())
+                .foregroundColor(.white)
+                .padding(.bottom, 60)
+    }
+    
+    private var flagStack: some View {
+        VStack(spacing: 15) {
+            ForEach(0..<3) { number in
+                Button {
+                    vm.flagTapped(number)
+                } label: {
+                    Image(vm.countries[number])
+                        .flagStyle()
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 20)
+        .background(.regularMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+    
+    private var scoreView: some View {
+        Text("Puntuación: \(vm.score)")
+            .foregroundColor(.white)
+            .font(.title.bold())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
